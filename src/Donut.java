760dbfc65922430008c6148abe8e1dec31d76401
Jose Luis de la asunción <jose.delaasuncion@alumnos.uneatlantico.es>
2025-04-14 00:07:35 +0000

    FInalizado

--- src/Donut.java
+++ src/Donut.java
@@ -7,48 +7,54 @@ public class Donut {
         final double PASO_THETA = 0.02;
         final double PASO_PSI = 0.02;
         final double DISTANCIA_DONUT = 15;
+        final double ESCALA_DONUT = 4;
+        final int HEIGHT = 28;
+        final int WIDTH = 60;
 
-        double rotacionX = 0;
-        char[][] pantalla = new char[24][80];
+        double rotacionEjeX = 0;
+        double rotacionEjeZ = 0;
+        char[][] pantalla = new char[HEIGHT][WIDTH];
+        double[][] zBuffer = new double[HEIGHT][WIDTH];
 
         while (true) {
-            for (int y = 0; y < pantalla.length; y++) {
-                for (int x = 0; x < pantalla[0].length; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
                     pantalla[y][x] = ' ';
+                    zBuffer[y][x] = 0;
                 }
             }
 
-            // Dibujar el donut
             for (double theta = 0; theta < 2 * Math.PI; theta += PASO_THETA) {
                 for (double psi = 0; psi < 2 * Math.PI; psi += PASO_PSI) {
-                    double x = (RADIO_SUPERIOR + RADIO_INFERIOR * cos(theta)) * cos(psi);
-                    double y = (RADIO_SUPERIOR + RADIO_INFERIOR * cos(theta)) * sin(psi);
-                    double z = RADIO_INFERIOR * sin(theta);
+                    double circuloX = RADIO_SUPERIOR + RADIO_INFERIOR * cos(theta);
+                    double circuloY = RADIO_INFERIOR * sin(theta);
 
-                    double yRotada = y * cos(rotacionX) - z * sin(rotacionX);
-                    double zRotada = z * cos(rotacionX) + y * sin(rotacionX);
+                    double x = circuloX * (float)(Math.cos(rotacionEjeZ) * cos(psi) + Math.sin(rotacionEjeX) * Math.sin(rotacionEjeZ) * sin(psi)) - circuloY * (float)Math.cos(rotacionEjeX) * Math.sin(rotacionEjeZ);
+                    double y = circuloX * (float)(Math.sin(rotacionEjeZ) * cos(psi) - Math.sin(rotacionEjeX) * Math.cos(rotacionEjeZ) * sin(psi)) + circuloY * (float)Math.cos(rotacionEjeX) * Math.cos(rotacionEjeZ);
+                    double z = ESCALA_DONUT + Math.cos(rotacionEjeX) * circuloX * sin(psi) + circuloY * Math.sin(rotacionEjeX);
 
-                    // Proyección y manejo de valores pequeños de zRotada
-                    double factor = DISTANCIA_DONUT / Math.max(Math.abs(zRotada), 0.1);
-                    double[] pantallaCordenadas = {x * factor, yRotada * factor};
+                    double zInverso = 1/z;
+                    double luz = cos(psi) * cos(theta) * Math.sin(rotacionEjeZ) - Math.cos(rotacionEjeX) * cos(theta) * sin(psi) - Math.sin(rotacionEjeX) * sin(theta) + Math.cos(rotacionEjeZ) * (Math.cos(rotacionEjeX) * sin(theta) - cos(theta) * Math.sin(rotacionEjeX) * sin(psi));
 
-                    // Escalar las coordenadas de la pantalla
-                    double escalaX = (pantalla[0].length / 6.0);
-                    double escalaY = (pantalla.length / 6.0);
+                    int xProyectada = (int)(WIDTH / RADIO_SUPERIOR + DISTANCIA_DONUT * x / z);
+                    int yProyectada = (int)(HEIGHT / RADIO_SUPERIOR - DISTANCIA_DONUT * y / z);
 
-                    int xPantalla = (int) (escalaX + pantallaCordenadas[0]);
-                    int yPantalla = (int) (escalaY - pantallaCordenadas[1]);
-
-                    // Asegurarse de que las coordenadas estén dentro de los límites
-                    if (yPantalla >= 0 && yPantalla < pantalla.length && xPantalla >= 0 && xPantalla < pantalla[0].length) {
-                        pantalla[yPantalla][xPantalla] = '*';
+                    if (xProyectada >= 0 && xProyectada < WIDTH && yProyectada >= 0 && yProyectada < HEIGHT && zInverso > zBuffer[yProyectada][xProyectada]) {
+                        zBuffer[yProyectada][xProyectada] = zInverso;
+                        int indiceLuminiscencia = (int)(luz * 8);
+                        final String caracteresLuminicos = ".,-~:;=!*#$@";
+                        char caracterFinal = (indiceLuminiscencia >= 0 && indiceLuminiscencia < caracteresLuminicos.length())
+                                  ? caracteresLuminicos.charAt(indiceLuminiscencia)
+                                  : ' ';
+                        pantalla[yProyectada][xProyectada] = caracterFinal;
                     }
                 }
             }
 
             imprimirPantalla(pantalla);
             Thread.sleep(50);
-            rotacionX+=0.04;
+            rotacionEjeX+=0.04;
+            rotacionEjeZ+=0.04;
         }
     }
 
